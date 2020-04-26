09158b499d80c3bb219451f495f1697dc626a50d
itskhanarif <contact.arifuzzaman@gmail.com>
2020-04-26 16:57:11 +0600

    blog post with owl carousel completed & all images changed

--- js/main.js
+++ js/main.js
@@ -3,8 +3,17 @@ $(document).ready(function(){
     $nav = $('.nav');
     $toggleCollapse = $('.toggle-collapase');
 
+    //Toggling the nav manu
     $toggleCollapse.click(function(){
       $nav.toggleClass('collapse');
     })
-
+    //owl carousel
+    $('.owl-carousel').owlCarousel({
+      loop: true,
+      autoplay: true,
+      autoplayTimeout: 3000,
+      dots : false,
+      nav: true,
+      navText: [$('.owl-navigation .owl-nav-prev'), $('.owl-navigation .owl-nav-next')]
+    });
 });
